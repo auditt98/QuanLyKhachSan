5d22914784443aaa54fcb5bf4d50d74f5973dce6
auditt98 <vietanh8i1998@gmail.com>
2020-05-24 21:38:56 +0700

    ty

--- QLKS/Domain/LOAIPHONG.cs
+++ QLKS/Domain/LOAIPHONG.cs
@@ -9,17 +9,13 @@ namespace QLKS.Domain
     [Table("LOAIPHONG")]
     public partial class LOAIPHONG
     {
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
         public LOAIPHONG()
         {
             PHONGs = new HashSet<PHONG>();
         }
 
-<<<<<<< HEAD:QuanLyKhachSan/Models/LOAIPHONG.cs
         [Key]
        
-=======
->>>>>>> 749f76a4fdc70fed2b65ebfa198eb96eaca954e4:QLKS/Models/LOAIPHONG.cs
         public int ID { get; set; }
 
         [Required]
@@ -28,7 +24,6 @@ namespace QLKS.Domain
 
         public string ghichu { get; set; }
 
-<<<<<<< HEAD:QuanLyKhachSan/Models/LOAIPHONG.cs
         public string anh { get; set; }
 
         public string khungnhin { get; set; }
@@ -44,29 +39,9 @@ namespace QLKS.Domain
         public string tiennghi { get; set; }
 
         public string thongtin { get; set; }
-=======
-        [StringLength(20)]
+
         public string ma { get; set; }
->>>>>>> 749f76a4fdc70fed2b65ebfa198eb96eaca954e4:QLKS/Models/LOAIPHONG.cs
 
-        [StringLength(500)]
-        public string anh { get; set; }
-
-        [StringLength(100)]
-        public string khungnhin { get; set; }
-
-        public int? dientich { get; set; }
-
-        [StringLength(100)]
-        public string giuong { get; set; }
-
-        public int? nguoilon { get; set; }
-
-        public int? trecon { get; set; }
-
-        public string thongtin { get; set; }
-
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<PHONG> PHONGs { get; set; }
     }
 }
