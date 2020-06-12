2bae418b7cd85bd232022279fb071b02b9ee422e
auditt98 <vietanh8i1998@gmail.com>
2020-06-12 20:18:50 +0700

    m

---
+++ QLKS/Controllers/LuuTruController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Ajax.Utilities;
+using QLKS.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QLKS.Controllers
+{
+    public class LuuTruController : Controller
+    {
+        // GET: LuuTru
+        QLKSContext db = new QLKSContext();
+        public ActionResult List()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult PopulateLuuTru()
+        {
+            var danhSachNhomNguoiDung = db.LUUTRUs.Select(c => new
+            {
+                loaihanhdong = c.loaihanhdong,
+                ngaychinhsua = c.ngaychinhsua,
+                tennguoidung = c.NGUOIDUNG.tennguoidung,
+                ghichu = c.ghichu,
+                uid = c.ID
+            }).OrderBy(c => c.uid).ToList();
+            var result = new { data = danhSachNhomNguoiDung };
+            return Json(result);
+        }
+    }
+}
